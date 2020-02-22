4bef6c64198a8c5d181c90d745d81561ffa8c52a
Crystal Mulluns <stardustCrystal@users.noreply.github.com>
2020-02-21 23:26:29 -0800

    second commit

---
+++ week2/if_statements_olympics.js
@@ -0,0 +1,99 @@
+// prelimanary if statments olympics
+if (true) {
+    ( 5 > 3 )
+ }
+ 
+ console.log ("is greater than")
+ 
+ if (true) {
+      ("cat" === 3)
+ }
+ 
+ console.log ("is the lenght")
+ 
+ if (false) {
+     ("cat" === "dog")
+ }
+ console.log ("not the same")
+ // bronze medel: object with values and if statments
+ var human = {
+     name: "Robbie",
+     age: 18,
+ }
+     console.log(human.age)
+ 
+     if (human.age >= 18 && human.name.startsWith('B')) {
+  
+      console.log("Bobbie is allowed to go to the movie")
+ 
+   } else  {
+ 
+        console.log("Bobbie is not allowed to go to the movie")
+   } 
+ 
+//    silver medal
+ 
+   if (1 === "1" || 1 == "1"){
+ 
+         console.log("strict", "loose")
+ 
+ }
+ else if (1 !== "1"){ 
+ 
+        console.log("not equal at all")
+ }
+ 
+ // ternary syntax
+ 
+ const int_value = (1 === "1") ? true: false; 
+ 
+     console.log("not equal at all")
+
+    //  Gold Medal
+// 1
+    if ("dog" === "dog")
+ {
+     console.log("dog is a string!");
+ }
+     
+// 2
+ if (true === true)
+ {
+     console.log("true is a boolean")
+ }
+
+
+
+ 
+// 4 & 5
+ if ("s" > 12)
+ {
+     console.log("it is not greater")
+
+}
+if ("sa" > 1)
+ {
+     console.log("it is not greater")
+
+ }
+
+ if ("w" > 3)
+ {
+     console.log("it is not greater")
+
+ }
+
+else ("s" === "s")
+{
+    console.log("they are the same")
+}
+ 
+// 6 ternary statement to check for odd/even number
+var num = 7
+
+if ( num % 2 == 0) {
+	console.log("Even Number");
+}else
+{
+	console.log("Odd Number");
+}
