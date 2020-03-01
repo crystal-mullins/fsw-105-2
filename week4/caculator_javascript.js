285399afa070513e600f40f835675366225b4433
Crystal Mulluns <stardustCrystal@users.noreply.github.com>
2020-02-29 22:46:20 -0800

    saving for later

---
+++ week4/caculator_javascript.js
@@ -0,0 +1,18 @@
+const readlineSync = require('readline-sync');
+
+//  caculator in javascript #1 add, 2 num with readline-sync
+function addTwoNumbers(num1, num2)
+{
+    return "Adding" + num1 + " with" + num2 + " gives you a result of" + num1 + num2;
+}
+var firstNumber = readlineSync.questionInt("Please a number")
+var secondNumber = readlineSync.questionInt("Please another number");
+var  mathOperator = readlineSync.question("Please choose an operator: add, sub, mul, div,");
+
+if (mathOperator == "add")
+{
+        console.log(addTwoNumbers(num1, num2));
+} else if (mathOperator == "sub")
+{
+    console.log(addTwoNumbers(num1, num2));
+}
