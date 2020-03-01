a9e58fc00d3218fb135f7b687d034661c5689a77
Crystal Mulluns <stardustCrystal@users.noreply.github.com>
2020-02-29 20:56:58 -0800

    first push

--- week3/functions_folder/functions.js
+++ week3/functions_folder/functions.js
@@ -13,23 +13,23 @@ console.log(result)
 
 // functions # 2 find the largest of three numbers 
 
-function largestNumber(num1, num2, num3){
+// function largestNumber(num1, num2, num3){
      
-    if (num1 > num2 && num3){
-        return "value " + num1 + " is the largest"
-    }  else if (num2 > num1 && num3){
+//     if (num1 > num2 && num3){
+//         return "value " + num1 + " is the largest"
+//     }  else if (num2 > num1 && num3){
 
-            return "value " + num2 + " is the largest"
-       }   else if (num3 > num1 && num2){
-           return "value " + num3 + " is the largest"
-       }
-}
+//             return "value " + num2 + " is the largest"
+//        }   else if (num3 > num1 && num2){
+//            return "value " + num3 + " is the largest"
+//        }
+// }
 
 
-var num1 = 5
-var num2 = 7
-var num3 = 4
-console.log(largestNumber(5, 7, 4))
+// var num1 = 5
+// var num2 = 7
+// var num3 = 4
+// console.log(largestNumber(5, 7, 4))
      
 
 // accepts 1 number and returns if odd or even
--- week3/loops_arrays_pratice.js
+++ week3/loops_arrays_pratice.js
@@ -1,86 +1,83 @@
-// first loop
+// first loop how many computers in list
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp","stapler", "computer", "computer"]
 
-var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler","computer", "computer"]
-function countItemsOnList(nameOfList, itemsToBeSearched){
-            var numberOfItems = 0;
-            for (i = 0; i < nameOfList.length; i++)
-            {
-                if(nameOfList[i] == itemsToBeSearched)
-                {
-                    numberOfItems = numberOfItems + 1;
-                }
-                
-            }
-            console.log("final count:"  + numberOfItems);
+function countItemsOnList(nameOfList, itemToBeSearchd){
+    var numberOfItems = 0;
+    for (i = 0; i < nameOfList.length; i++)
+    {
+        if(nameOfList[i] == itemToBeSearchd)
+        {
+            numberOfItems = numberOfItems + 1;
+        }
+
+    }
+    console.log("final count: "+ numberOfItems + " computers");
 }
+countItemsOnList(officeItems, "computer")
 
-countItemsOnList(officeItems, "computer");
 
 // javascript loops #2
 
-// var peopleWhoWantToSeeMadMaxFuryRoad = [
-//     {
-//         name: "Crystal",
-//         age: 38,
-//         gender: "female",
-//     },{
-//         name: "Trinity",
-//         age: 7,
-//         gender: "female"
-//     },{
-//         name: "Adawehi",
-//         age: 14,
-//         gender: "female"
-// },{
-//         name: "Clarity",
-//         age: 23,
-//         gender: "female",
-// },{
-//         name: "Cyperus",
-//         age: 20,
-//         gender: "male"
-// }  
-// ]
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+    {
+        name: "Crystal",
+        age: 38,
+        gender: "female",
+    },{
+        name: "Trinity",
+        age: 7,
+        gender: "female"
+    },{
+        name: "Adawehi",
+        age: 14,
+        gender: "female"
+},{
+        name: "Clarity",
+        age: 23,
+        gender: "female",
+},{
+        name: "Cyperus",
+        age: 20,
+        gender: "male"
+}  
+]
+// #2 function they can or they cannot go to the movies
 
-// function getIdAge(age)
-//     for (i = 0; i < 18; i ++){
+var age = ''
+for (var a = 0; a < peopleWhoWantToSeeMadMaxFuryRoad.length; a++){
+    if (peopleWhoWantToSeeMadMaxFuryRoad[a].age < 18){
+        console.log("you are not allowed to go to the movies") 
 
-//     if (i <= 18){
-//         console.log("you are not allowed to go to the movies")
-//     }
-//     else {
-//         console.log("you are not allowed to go to the movies")
-//     }
-// }
-    
-
-
-
-// console.log(peopleWhoWantToSeeMadMaxFuryRoad["age"])
-
-    
-// console.log(peopleWhoWantToSeeMAdMaxFuryRoad.name)
-// console.log(peopleWhoWantToSeeMAdMaxFuryRoad.age)
-
-
-// console.log("Crystal is allowed to go to the movies")
-// console.log("Clarity is allowed to go to the movies")
-// console.log("Adawehi is not allowed to go to the movies")
-// console.log("Cyperus is allowed to go to the movies")
-// console.log("Trinity is not allowed to go to the movies");
-
-var counter = 1
-function countTo101(counter, declareOddEven){
-
-for (i = 0; i > 101; i ++){
-    if(i % 2 == 0){
-    console.log("even" + i)
+    }  else if (peopleWhoWantToSeeMadMaxFuryRoad[a].age >= 18){
+        console.log("you are allowed to go to the movies")
     }
-     else {
-        console.log("odd" + i)
-    }
-
 }
+// #3 function with name and gender 
+var gender = ''
+for (var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
+    if (peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "male"){
+        gender = "him"
+    } else {
+        gender = "her"
+    }
+    if (peopleWhoWantToSeeMadMaxFuryRoad[i].age < 18){
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enought to see Mad Max!" + " do not let " + gender + " in!!")
+    } else if (peopleWhoWantToSeeMadMaxFuryRoad[0].age > 18){
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enought to see Mad Max! " + " let " + gender + " in!!")
+    }
 }
 
-// (i % 2 == 0 ? console.log("even number"): console.log("odd number"));
+    // functions # 5 return if a number is odd or even from 0 to 100
+
+var i
+
+for (i = 0; i < 101; i++)
+{   
+     if(i % 2 === 0){
+    console.log("even")
+    }
+     else if (i % 2 !== 0)
+     {
+        console.log("odd")
+    } 
+}
